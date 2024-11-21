ada988abf64b100a4e0febd23d3cc367499d8b80
apigap01 <abigail.prescott@phs.scot>
2024-11-21 15:43:30 +0000

    Update chart to highcharter from Plotly

--- app.R
+++ app.R
@@ -7,7 +7,8 @@
 ##Packages 
 
 library(dplyr) #data manipulation
-library(plotly) #charts
+library(highcharter) #charts
+library(phsstyles) #for chart colors
 library(shiny) #shiny app
 library(tidyr)
 library(readr) #for reading in csv
@@ -65,12 +66,6 @@ hep_c <- readRDS(paste0(filepath, "/shiny_data_hepatitisc_board.rds")) #reading 
 #Use for selection of areas
 board_list <- sort(unique(hep_c$nhsboard[hep_c$nhsboard != "Scotland"]))
 
-#ScotPHO logo. 
-#Needs to be https address or if local in code 64 (the latter does not work with 4.7 plotly)
-scotpho_logo <-  list(source ="https://raw.githubusercontent.com/ScotPHO/plotly-charts/master/scotpho.png",
-                      xref = "paper", yref = "paper",
-                      x= -0.09, y= 1.2, sizex = 0.22, sizey = 0.18, opacity = 1)
-
 ############################.
 ## Visual interface ----
 ############################.
@@ -88,13 +83,15 @@ ui <- fluidPage(style="width: 650px; height: 500px; ",
                             choices = board_list))
                 ),
                 div(style= "width:100%; float: left;", #Main panel
-                  plotlyOutput("chart", width = "100%", height = "350px"),
+                  highchartOutput("line_chart"),
+                  #plotlyOutput("chart", width = "100%", height = "350px"),
                   p(div(style = "width: 25%; float: left;", #Footer
-                        HTML("Source: <a href='https://hpspubsrepo.blob.core.windows.net/hps-website/nss/2834/documents/1_hcv-testing-diagnosis-treatment-scotland-2018.pdf' target='_blank'>HPS</a>")),
+                        HTML("Source: <a href='https://hpspubsrepo.blob.core.windows.net/hps-website/nss/2834/documents/1_hcv-testing-diagnosis-treatment-scotland-2018.pdf' target='_blank'>HPS</a> (2009-18)"),
+                        HTML("Source: <a href='https://www.publichealthscotland.scot/publications/surveillance-of-hepatitis-c-in-scotland/surveillance-of-hepatitis-c-in-scotland-progress-on-elimination-of-hepatitis-c-as-a-major-public-health-concern-2023-update/' target='_blank'>PHS</a> (2021-)")),
                     div(style = "width: 25%; float: left;",
                         downloadLink('download_data', 'Download data')),
                     div(style = "width: 50%; float: left;",
-                        "Note: Year of earliest positive specimen.")
+                        "Notes: Year of earliest positive specimen. Publication of 2019 and 2020 data was prevented by the COVID-19 pandemic.")
                         )
                   )
                 )
@@ -109,47 +106,35 @@ server <- function(input, output) {
     filename =  'hepatitisc_data.csv', content = function(file) { 
       write.csv(hep_c, file, row.names=FALSE) })
   
-  ############################.
-  #Visualization
-  output$chart <- renderPlotly({
-    #For Island plots and rates plot an empty chart
-    if (input$area == "Island Boards" & input$measure == "Rate") {
-        text_na <- list(x = 5, y = 5, text = "Rates are not published for the island boards" ,
-                        xref = "x", yref = "y",  showarrow = FALSE, size=15)
-        plot_ly() %>%
-          layout(annotations = text_na,
-                 yaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
-                 xaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
-                 font = list(family = 'Arial, sans-serif')) %>% 
-          config( displayModeBar = FALSE) # taking out plotly logo and collaborate button
-      } else {
-
-    #Data for Scotland line
-    data_scot <- hep_c %>% subset(nhsboard=="Scotland" & measure==input$measure)
-    #Data for Health board line
-    data_board <- hep_c %>% subset(nhsboard==input$area & measure==input$measure)
+  #Creating chart 
+  output$line_chart <- renderHighchart({
+    
+    #Data for plot
+    data_chart <- hep_c |> subset(measure == input$measure & nhsboard == input$area)
+    
+    #Separate Scotland data out for separate series
+    data_scot <- hep_c |> subset(measure == input$measure & nhsboard == "Scotland")
     
     #y axis title
-    yaxistitle <- ifelse(input$measure == "Rate", "Rate per 100,000", "Number of diagnosis")
+    yaxistitle <- ifelse(input$measure == "Number", "Number of diagnoses",
+                         "Rate per 100,000")
     
-    plot <- plot_ly(data=data_board, x=~year, y = ~value, 
-                    type = "scatter", mode = 'lines',  line = list(color = '#08519c'),
-                    name = unique(data_board$nhsboard), width = 650, height = 350) %>% 
-      add_lines(data = data_scot, y = ~value, mode = 'lines', 
-                name = "Scotland", line = list(color = '#000000')) %>%
-    #Layout
-      layout(annotations = list(), #It needs this because of a buggy behaviour
-           yaxis = list(title = yaxistitle, rangemode="tozero", fixedrange=TRUE), 
-           xaxis = list(title = "Year",  fixedrange=TRUE),  
-           font = list(family = 'Arial, sans-serif'), #font
-           margin = list(pad = 4, t = 50), #margin-paddings
-           hovermode = 'false',  # to get hover compare mode as default
-           images = scotpho_logo) %>% 
-      config(displayModeBar= T, displaylogo = F, collaborate=F, editable =F) # taking out plotly logo and collaborate button
-      }
-    }) 
+    #Creating dynamic text for if Island Board rates selected
+    validate(
+      need((input$area != "Island Boards" | input$measure == "Number"), "Rates are not published for the island boards"))
+    
+    highchart() |>
+      hc_add_series(data_chart, "line", hcaes(y = value, x = year), name = input$area) |>  
+      hc_add_series(data_scot, "line", hcaes(y = value, x = year), name = "Scotland") |>  
+      hc_xAxis(title = list(text = "Year")) |> 
+      hc_yAxis(title = list(text = yaxistitle)) |> 
+      hc_legend(align = "left", verticalAlign = "top")
+    
+  })
   
   } # end of server part
+
+
 
 ############################.
 ## Calling app ----
